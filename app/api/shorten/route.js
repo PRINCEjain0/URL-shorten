d01449f4dd2b5350e77039121aa0a189b3eda84d
Prince Jain <pj122333444@gmail.com>
2025-02-04 23:34:38 +0530

    changes

--- app/api/shorten/route.js
+++ app/api/shorten/route.js
@@ -15,7 +15,7 @@ export async function POST(req) {
       },
     });
 
-    return Response.json({ shortUrl: `http://shorturl/${shortUrl}` });
+    return Response.json({ shortUrl: `http:/localhost:3001/${shortUrl}` });
   } catch (error) {
     return Response.json({ error: error }, { status: 500 });
   }
